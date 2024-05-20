ac2d93b40318432d6a2fa1a1d23ec33c66089e73
ndz <nicolas.dzierzgowski@gmail.com>
2024-05-20 16:18:22 +0200

    created nav-bar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,25 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { UserPageComponent } from './components/user-page/user-page.component';
+import { GroceryListComponent } from './components/grocery-list/grocery-list.component';
+import { PlanningComponent } from './components/planning/planning.component';
+
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
+      },
+      {
+        path: 'user',
+        component: UserPageComponent,
+      },
+      {
+        path: 'list',
+        component: GroceryListComponent,
+      },
+      {
+        path: 'planning',
+        component: PlanningComponent,
       }
 ];
