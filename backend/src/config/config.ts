c47e83bcb4f2f66fee568ee5d730855feb951bd8
ndz <nicolas.dzierzgowski@gmail.com>
2024-07-08 16:00:38 +0200

    ugly working version

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -3,5 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const DB_URI = String(process.env.DB_URI);
-export const JWT_SECRET = String (process.env.JWT_SECRET);
-
+export const JWT_SECRET = String(process.env.JWT_SECRET);
+export const CLOUD_NAME = String(process.env.CLOUD_NAME);
+export const CLOUDINARY_KEY = String(process.env.CLOUDINARY_KEY);
+export const CLOUDNIARY_SECRET = String(process.env.CLOUDNIARY_SECRET);
