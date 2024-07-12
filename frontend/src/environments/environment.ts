7d7086c49993f84458685c82f27c7bb6926ee8ca
ndz <nicolas.dzierzgowski@gmail.com>
2024-07-12 15:16:56 +0200

    fixing the same old url ^^'

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://ultimate-cookbook-8beb64e72401.herokuapp.com/',
+  apiUrl: 'https://ultimate-cookbook-8beb64e72401.herokuapp.com',
 };
